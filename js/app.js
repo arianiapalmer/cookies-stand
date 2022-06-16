66694e68d3de2262290c1d3dd9447cbb1261114e
arianiapalmer <arianiapalmer@gmail.com>
2022-06-16 12:50:37 -0400

    took care of some spelling

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm', '5pm', '6pm', '7pm']
+const hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm', '4pm', '5pm', '6pm', '7pm'];
 
 function generateNumberBetween(min, max){
     return Math.round(Math.random() + (max-min)) + min;
 }
 function genCustPerHour(){
-    for (let i = 0; i < hours.length; i++){
+    for (let i = 0; i < hours.lengt h; i++){
         console.log(generateNumberBetween(this.minCust, this.maxCust));
         return[genCustPerHour];
     }
 }
 
-const Seattle = {
+const seattle = {
     minCust: 23,
     maxCust: 65,
     avgCookiesPerCust: 6.3,
@@ -22,7 +22,7 @@ const Seattle = {
 
 seattle.genCustPerHour();
 
-const Tokyo = {
+const tokyo = {
     minCust: 3,
     maxCust: 24,
     avgCookiesPerCust: 1.2,
@@ -30,7 +30,7 @@ const Tokyo = {
         
     }
 }
-const Dubai = {
+const dubai = {
     minCust: 11,
     maxCust: 38,
     avgCookiesPerCust: 3.7,
@@ -38,7 +38,7 @@ const Dubai = {
         
     }
 }
-const Paris = {
+const paris = {
     minCust: 20,
     maxCust: 38,
     avgCookiesPerCust: 2.3,
@@ -46,7 +46,7 @@ const Paris = {
         
     }
 }
-const Lima = {
+const lima = {
     minCust: 2,
     maxCust: 16,
     avgCookiesPerCust: 4.6,
