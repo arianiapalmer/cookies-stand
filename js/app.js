365b4e1dafb8f06fae675e2cb21a67e4285f49ce
arianiapalmer <arianiapalmer@gmail.com>
2022-07-06 18:43:52 -0400

    rendered table data onto sales.html

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ function Store(name, minCust, maxCust, avgCookiePerCust) {
   this.totalSales = 0;
   allStores.push(this);
   this.cookiesPerHour();
-  this.displaySales();
 };
 
 Store.prototype.randomCustomerHR = function () {
@@ -29,27 +28,50 @@ Store.prototype.cookiesPerHour = function () {
 };
 
 
-Store.prototype.displaySales = function () {
+function displaySales() {
 
   let resultsTable = document.getElementById('results');
-  let tableRow = document.createElement('tr');
-  resultsTable.appendChild(tableRow);
-  let hoursHead = document.createElement('th');
-  hoursHead.textContent = "Hours"
+  let hoursHead = document.createElement('tr');
+  let hoursTH = document.createElement('th');
+  hoursTH.textContent = 'Hours'
+  hoursHead.appendChild(hoursTH);
+  // let tableRow = document.createElement('tr');
+  // resultsTable.appendChild(tableRow);
+  // hoursHead.textContent = "Hours"
   // tableHead.textContent = hours[]; create for loop
 
   for(let i = 0; i < hours.length; i++){
 
-    let hourData = document.createElement('td');
+    let hourData = document.createElement('th');
     hourData.textContent = hours[i];
     hoursHead.appendChild(hourData);
-    tableRow.appendChild(hoursHead);
+    // tableRow.appendChild(hoursHead);
   }
+  let totalsTd = document.createElement('th');
+  totalsTd.textContent = "Totals";
+  hoursHead.appendChild(totalsTd)
+  resultsTable.appendChild(hoursHead);
+
   for (let i = 0; i < allStores.length; i++){
+
     let storeRow = document.createElement('tr');
-    storeRow.textContent = `${allStores[0].name}`
-    tableRow.appendChild(storeRow);
+    let storeNameTd = document.createElement('td');
+    
+    storeNameTd.textContent = allStores[i].name;
+    storeRow.appendChild(storeNameTd);
+    console.log(allStores[i])
+    for (let j = 0; j < hours.length; j++){
+      let hourlyData = document.createElement('td');
+      hourlyData.textContent = allStores[i].cookiesSoldPerHr[j];
+      storeRow.appendChild(hourlyData);
+    }
+
+    let storesTotalCell = document.createElement('td');
+    storesTotalCell.textContent = allStores[i].totalSales;
+    storeRow.appendChild(storesTotalCell);
+    resultsTable.appendChild(storeRow)
   }
+
   // for (let i = 0; i < hours.length; i++){
    
   //   let storeData = document.createElement('td');
@@ -68,6 +90,6 @@ new Store('Dubai', 11, 38, 3.7);
 new Store('Paris', 20, 38, 3.7);
 new Store('Lima', 2, 16, 4.6);
 
-console.log(allStores)
+displaySales();
 
 
